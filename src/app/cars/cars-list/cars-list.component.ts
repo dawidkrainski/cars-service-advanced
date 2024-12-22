b82acb260e1560aed3cf49fb9246781e0a77a218
dawid <d.krainski@cez.gov.pl>
2024-12-22 18:23:11 +0100

    first commit

---
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit,AfterViewInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import {Car} from "../models/car";
+import {TotalCostComponent} from "../total-cost/total-cost.component";
+import {CarsService} from "../cars.service";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CostSharedService} from "../cost-shared.service";
+
+@Component({
+  selector: 'cs-cars-list',
+  templateUrl: './cars-list.component.html',
+  styleUrls: ['./cars-list.component.less'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CarsListComponent implements OnInit {
+  @ViewChild("totalCostRef") totalCostRef : TotalCostComponent;
+  totalCost : number;
+  grossCost : number;
+  cars : Car[] = [];
+  carForm : FormGroup;
+
+  constructor(private carsService : CarsService,
+              private formBuilder : FormBuilder,
+              private costSharedService : CostSharedService,
+              private router : Router) {}
+
+  ngOnInit() {
+    this.loadCars();
+    this.carForm = this.buildCarForm();
+  }
+
+  buildCarForm() {
+    return this.formBuilder.group({
+      model: ['', Validators.required],
+      type: '',
+      plate: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(7)]],
+      deliveryDate: '',
+      deadline: '',
+      color: '',
+      power: '',
+      clientFirstName: '',
+      clientSurname: '',
+      cost: '',
+      isFullyDamaged: '',
+      year: ''
+    });
+  }
+
+  loadCars() : void {
+    this.carsService.getCars().subscribe((cars) => {
+      this.cars = cars;
+      this.countTotalCost();
+      this.costSharedService.shareCost(this.totalCost);
+    });
+  }
+
+  addCar() {
+    this.carsService.addCar(this.carForm.value).subscribe(() => {
+      this.loadCars();
+    });
+  }
+
+  goToCarDetails(car : Car) {
+    this.router.navigate(['/cars', car.id]);
+  }
+
+  removeCar(car : Car, event) {
+    event.stopPropagation();
+    this.carsService.removeCar(car.id).subscribe(() => {
+      this.loadCars();
+    });
+  }
+
+  showGross() : void {
+    this.totalCostRef.showGross();
+  }
+
+  countTotalCost() : void {
+    if (this.cars.length === 0) {
+      return;
+    }
+
+    this.totalCost = this.cars
+      .map((car) => car.cost)
+      .reduce((prev, next) => prev + next);
+  }
+
+  onShownGross(grossCost : number) : void {
+    this.grossCost = grossCost;
+  }
+}
